66bedc0a674d209c3c4a49f4ec22293801d1d331
HanterGER <hanswuehling@gmail.com>
2020-04-05 20:39:15 +0200

    äölkjhgfd

--- src/main/dataclicker/buyers/Buyers_Template.java
+++ src/main/dataclicker/buyers/Buyers_Template.java
@@ -1,15 +1,31 @@
 package main.dataclicker.buyers;
 
-public class Buyers_Template {
-    private String name;
-    private int level;
-    private int preis;
-    private int gewinn;
+import main.dataclicker.player.Player;
 
-    public Buyers_Template() {
-    }
-
-    public void kauf() {
-        throw new Error("Unresolved compilation problems: \n\tdataAmount cannot be resolved to a variable\n\tdataAmount cannot be resolved to a variable\n\tdataAmount cannot be resolved to a variable\n\tamountOfMoney cannot be resolved to a variable\n\tamountOfMoney cannot be resolved to a variable\n");
-    }
+public class Buyers_Template 
+{
+	private String name;
+	private int level;
+	private int preis;
+	private int gewinn;
+	//private int dataAmount;
+	//private int moneyAmount;
+	
+	public Buyers_Template(String name, int preis, int gewinn)
+	{
+		this.name = name;
+		this.level = 1;
+		this.preis = preis;
+		this.gewinn = gewinn;
+	}
+	public void kauf()
+	{
+		if(player.getdataAmount() >= preis)
+		{
+			level = level + 1;
+			dataAmount = dataAmount - preis;
+			preis = preis * 2;
+			moneyAmount = moneyAmount + gewinn;
+		}	
+	}
 }
