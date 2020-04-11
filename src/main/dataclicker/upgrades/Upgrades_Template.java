ece3192f5c9371f058949adfa5da855b49f46e08
Aliegeus <tim.riemann2000@gmail.com>
2020-04-11 18:23:12 +0200

    First steps in Upgrades
    upgrades mk. I

--- src/main/dataclicker/upgrades/Upgrades_Template.java
+++ src/main/dataclicker/upgrades/Upgrades_Template.java
@@ -1,6 +1,21 @@
 package main.dataclicker.upgrades;
 
 public class Upgrades_Template {
+	private String upgradeName;
+	private String "upgradeDescription";
+	private int upgradeCost;
+	private boolean upgradeAvailable;
+	private boolean active;
+	
+	
+	
+	public boolean getActive() {
+		return active;
+	}
+	
+
+	
     public Upgrades_Template() {
+    
     }
 }
