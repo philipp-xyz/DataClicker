6d0ffb2627579cfdc8b1c5c5cc7289befb73b910
Aliegeus <tim.riemann2000@gmail.com>
2020-05-27 16:02:37 +0200

    1.why allways me that merges 2. Bit of slots
    joa ne ich hab da mal jetzt 1h20 min reininvestiert mein Layout from Scratch neu gemacht und hänge jetzt am übertragen der Slot ergebnise auf ihre Labels

--- src/main/dataclicker/minigames/dataSlots/SlotGui.java
+++ src/main/dataclicker/minigames/dataSlots/SlotGui.java
@@ -1,5 +1,12 @@
 package main.dataclicker.minigames.dataSlots;
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JButton;
+
+
+
 
 import javax.swing.GroupLayout;
 import javax.swing.JFrame;
@@ -9,21 +16,147 @@ import javax.swing.UIManager;
 
 public class SlotGui {
 
+	
 	public static int frameWidth = 500;  
 	public static int frameHeight = 500;
-	JFrame frame = new JFrame("DataSlots") ;
-	void setLayout(null);
-	JLabel label1= new JLabel();
 	
-	label1.setBounds(2,2,50,25); 
-	textField1.setBounds(10,2,50,25);
+	/*JFrame frame = new JFrame(); //LITERALLY COPPIED THIS IN FROM http://www.java2s.com/Code/Java/Swing-JFC/SettingtheStretchynessofRowsandColumnsinaGridBagLayoutUsingLayoutWeights.htm
+    GridBagLayout gbl = new GridBagLayout();	//AND STILL IT WONT WORK
+
+    frame.setLayout(gbl);
+    frame.add(new JButton("1"));
+    frame.add(new JButton("2"));
+
+    gbl.layoutContainer(frame);
+
+
+    gbl.columnWeights = new double[]{0.0f, 1.0f, 2.0f};
+    gbl.rowWeights = new double[]{0.0f, 1.0f};
+
+    frame.pack();
+    frame.setVisible(true);*/
+	
+	
+	
+	JFrame frame = new JFrame("DataSlots") ;
+	GridBagLayout gbl_slots = new GridBagLayout(); 
+	
+	
+	//frame.setLayout(gbl_slots);
+	
+	
+	 static int Gui_Slot1;
+     public int getGui_Slot1() {
+			return Gui_Slot1;
+		}
+     public static void setGui_Slot1(int variable) {
+    	 Gui_Slot1=variable;
+     }
+     static int Gui_Slot2;
+     public int getGui_Slot2() {
+			return Gui_Slot2;
+		}
+     public static void setGui_Slot2(int variable) {
+    	 Gui_Slot2=variable;
+     }
+     static int Gui_Slot3;
+     public int getGui_Slot3() {
+			return Gui_Slot3;
+		}
+     public static void setGui_Slot3(int variable) {
+    	 Gui_Slot3=variable;
+     }
+    
+	 public static void addComponentsToPane(Container pane) {
+         
+	       
+	         
+		 	JLabel label1= new JLabel("Win or Lose Label");
+	        pane.add(label1, BorderLayout.PAGE_START);
+	         
+	        
+	        
+	        JLabel label2 = new JLabel(""+Gui_Slot1);
+	        label2.setPreferredSize(new Dimension(166, 50));
+	        pane.add(label2, BorderLayout.LINE_START);
+	        
+	        JLabel label3 = new JLabel(""+Gui_Slot2);
+	        label3.setPreferredSize(new Dimension(166, 50));
+	        pane.add(label3, BorderLayout.CENTER);
+	         
+	        JLabel label4 = new JLabel(""+Gui_Slot3);
+	        label4.setPreferredSize(new Dimension(166, 50));
+	        pane.add(label4, BorderLayout.LINE_END);
+	        
+	        
+	        
+	        JButton button = new JButton("Start Knopf");
+	        pane.add(button, BorderLayout.PAGE_END);
+	        
+	 }  
+	 		public static void refreshLabels() {
+	 			JLabel label2 = new JLabel(""+Gui_Slot1);
+		        label2.setPreferredSize(new Dimension(166, 50));
+		        Slotsmain.pane.add(label2, BorderLayout.LINE_START);
+		        
+		        JLabel label3 = new JLabel(""+Gui_Slot2);
+		        label3.setPreferredSize(new Dimension(166, 50));
+		        Slotsmain.pane.add(label3, BorderLayout.CENTER);
+		         
+		        JLabel label4 = new JLabel(""+Gui_Slot3);
+		        label4.setPreferredSize(new Dimension(166, 50));
+		        Slotsmain.pane.add(label4, BorderLayout.LINE_END);
+	 		}
+	        public static void createAndShowGUI() {
+	            
+	            //Create and set up the window.
+	            JFrame frame = new JFrame("BorderLayoutDemo");
+	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	            //Set up the content pane.
+	            addComponentsToPane(frame.getContentPane());
+	            //Use the content pane's default BorderLayout. No need for
+	            //setLayout(new BorderLayout());
+	            //Display the window.
+	            frame.pack();
+	            frame.setVisible(true);
+	        }
+	        public static void main(String[] args) {
+	            /* Use an appropriate Look and Feel */
+	            
+	            /* Turn off metal's use bold fonts */
+	            UIManager.put("swing.boldMetal", Boolean.FALSE);
+	             
+	            //Schedule a job for the event dispatch thread:
+	            //creating and showing this application's GUI.
+	            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	                public void run() {
+	                    createAndShowGUI();
+	                }
+	            });
+	        }
+	        
+	         
+	        
+	    
+	//gbl_slots.layoutContainer(frame);
+	//gbl_slots.layoutContainer(frame);
+    
+	
+	//gbl_slots.columnWeights = new double[]{1.0f};
+	//gbl_slots.rowWeights = new double[] {0.0f,0.0f,0.0f};
+	
+	
 	
 	
 	public void Initialize() {
 		frame.setSize(frameWidth, frameHeight);
 		frame.setResizable(true);
 		frame.setVisible(true);}
-	
+		//frame.setLayout(gbl_slots);
+		//frame.setLayout(gbl_slots);
+		//gbl_slots.layoutContainer(frame);
+		
+		//gbl_slots.layoutContainer(frame);
 	
 	
 }
--- src/main/dataclicker/minigames/dataSlots/Slotsmain.java
+++ src/main/dataclicker/minigames/dataSlots/Slotsmain.java
@@ -1,11 +1,17 @@
 package main.dataclicker.minigames.dataSlots;
+import java.awt.Container;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Slotsmain {
+	
+	public static Container pane = new Container();
 	public static void main(String[] args) {
+		
+		SlotGui.addComponentsToPane(pane);
+		SlotGui.createAndShowGUI();
 		
 		Random rdm = new Random();
 		
@@ -18,6 +24,13 @@ public class Slotsmain {
 		    	int Slot1 = rdm.nextInt(9)+1;
 				int Slot2 = rdm.nextInt(9)+1;
 				int Slot3 = rdm.nextInt(9)+1;
+				
+				SlotGui.setGui_Slot1(Slot1);
+				SlotGui.setGui_Slot2(Slot2);
+				SlotGui.setGui_Slot3(Slot3);
+				SlotGui.refreshLabels();
+				
+				
 				if	(Slot1==7 && Slot3 ==7 && Slot2 ==7) {
 					System.out.println("Jackpot");}
 				else if (Slot1==7 && Slot2 ==7 || Slot2 == 7 && Slot3 ==7 || Slot1 ==7 && Slot3 == 7){
@@ -37,27 +50,35 @@ public class Slotsmain {
 					
 		    		 while (stop <=2){
 		    		 int Slot2 = rdm.nextInt(9)+1;
+		    		 SlotGui.setGui_Slot2(Slot2);
+		    		 SlotGui.refreshLabels();
 		    		 }
 		    		 while (stop <=2){
 		    			 int Slot3 = rdm.nextInt(9)+1;
-			    		 }
+		    			 SlotGui.setGui_Slot3(Slot3);
+		    			 SlotGui.refreshLabels();
+		    		 }
 		    		
+		    		
+						
+						
+		    		 
 		    		 stop=stop+1; 
 		    	 }
 		    	if (stop >=10) timer.cancel();
 		    		 
 		    }
 		},1*1000, 200);	
-		
-			
-			
-			
-			
-			
-		
-			
-		
-		
-		
 	}
+		
+			
+			
+			
+			
+		
+			
+		
+		
+		
+	
 }
