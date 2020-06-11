4968bf375ea3540637bb5628fdf11e4e40617fa7
Jamison <pflanzendrache@gmx.de>
2020-06-11 17:34:46 +0200

    optimizing for future use

--- src/main/dataclicker/minigames/DataPONG/Main.java
+++ src/main/dataclicker/minigames/DataPONG/Main.java
@@ -7,13 +7,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		initialize();
+
+	}
+	
+	public static void initialize() {
 		new Gui();
 		new Variables(); 
 		new Movement(); 	// exists for changing direction (request) --> done by a timer!
 		new Opponent_movement();
 		new Ball_movement();
 		new Ball_collision();
-
 	}
 
 }
